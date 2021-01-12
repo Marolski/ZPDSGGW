8e43d73123fccf347542abfde3a42aa7b3f0db1a
mbrzoska <mbrzoska@WRKPCSI68>
2021-01-12 19:02:54 +0100

    Initial upload file

---
+++ ZPDSGGW/Models/File.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZPDSGGW.Models
+{
+    public class File
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Extension { get; set; }
+        public string Description { get; set; }
+        public byte[] Content  { get; set; }
+    }
+}
---
+++ ZPDSGGW/Repository/IFileRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZPDSGGW.Models;
+
+namespace ZPDSGGW.Repository
+{
+    public interface IFileRepository
+    {
+        Task AddAsync(File file);
+        Task<File> GetFileByIdAsync(Guid id);
+        void Update(File file);
+    }
+}
